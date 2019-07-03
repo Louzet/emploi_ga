17f65c30ab9a2a0fafbb5087952b208509e148f2
Mickael <mlouzet@afrique-telecom.com>
2019-07-04 00:03:27 +0200

    (job) : mise en place du projet, creation de l'entity job

---
+++ src/Entity/Job.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Carbon\Carbon;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\JobRepository")
+ */
+class Job
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $priceMinimum;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $priceMaximum;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isPublished;
+
+    public function __construct(string $title, string $description)
+    {
+        $this->title = $title;
+        $this->description = $description;
+        $this->createdAt = Carbon::now();
+        $this->isPublished = false;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPriceMinimum(): ?int
+    {
+        return $this->priceMinimum;
+    }
+
+    public function setPriceMinimum(int $priceMinimum): self
+    {
+        $this->priceMinimum = $priceMinimum;
+
+        return $this;
+    }
+
+    public function getPriceMaximum(): ?int
+    {
+        return $this->priceMaximum;
+    }
+
+    public function setPriceMaximum(int $priceMaximum): self
+    {
+        $this->priceMaximum = $priceMaximum;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getIsPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setIsPublished(bool $isPublished): self
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+}
